2eb3a3bfbd5243f62e482f8a38dffca16d07cef5
amithkashyap <amithkashyaprv@gmail.com>
2024-06-12 14:36:20 +0530

    renamed product service component

--- src/main/java/com/ecommerce/productservice/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/productservice/service/ProductServiceImpl.java
@@ -3,7 +3,7 @@ package com.ecommerce.productservice.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProductService {
+public class ProductServiceImpl {
 
     public String getAllProducts() {
         return "all products";
