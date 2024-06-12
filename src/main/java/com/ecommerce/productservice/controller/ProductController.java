a93860eb0887a8311dec34f690606c2f5136237d
amithkashyap <amithkashyaprv@gmail.com>
2024-06-12 14:47:15 +0530

    injected service interface instead of concrete class

--- src/main/java/com/ecommerce/productservice/controller/ProductController.java
+++ src/main/java/com/ecommerce/productservice/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.productservice.controller;
 
+import com.ecommerce.productservice.service.ProductService;
 import com.ecommerce.productservice.service.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -8,35 +9,35 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ProductController {
 
-    private ProductServiceImpl productServiceImpl;
+    private ProductService productService;
 
     @Autowired
-    public ProductController(ProductServiceImpl productServiceImpl) {
-        this.productServiceImpl = productServiceImpl;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
     @GetMapping
     public String getAllProducts() {
-        return productServiceImpl.getAllProducts();
+        return productService.getAllProducts();
     }
 
     @GetMapping("/{id}")
     public String getProductById(@PathVariable("id") Long id) {
-        return productServiceImpl.getProductById(id);
+        return productService.getProductById(id);
     }
 
     @PostMapping("/")
     public String addProduct() {
-        return productServiceImpl.addProduct();
+        return productService.addProduct();
     }
 
     @PutMapping("/{id}")
     public String updateProduct(@PathVariable("id") Long id) {
-        return productServiceImpl.updateProduct(id);
+        return productService.updateProduct(id);
     }
 
     @DeleteMapping("/{id}")
     public String deleteProductById(@PathVariable("id") Long id) {
-        return productServiceImpl.deleteProductById(id);
+        return productService.deleteProductById(id);
     }
 }
