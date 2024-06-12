ce1b43d620b47919ec649f040d2ded8c8df5a898
amithkashyap <amithkashyaprv@gmail.com>
2024-06-12 13:47:12 +0530

    added initial product controller with dummy logic

--- src/main/java/com/ecommerce/productservice/controller/ProductController.java
+++ src/main/java/com/ecommerce/productservice/controller/ProductController.java
@@ -1,2 +1,24 @@
-package com.ecommerce.productservice.controller;public class ProductController {
+package com.ecommerce.productservice.controller;
+
+public class ProductController {
+
+    public String getAllProducts() {
+        return "all products";
+    }
+
+    public String getProductById(Long id) {
+        return "Product id : " + id;
+    }
+
+    public String addProduct() {
+        return "adding product";
+    }
+
+    public String updateProduct() {
+        return "updating product";
+    }
+
+    public String deleteProductById(Long id) {
+        return "deleting product : " + id;
+    }
 }
