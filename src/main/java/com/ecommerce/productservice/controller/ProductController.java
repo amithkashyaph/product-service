6e8b9c338cbee3ba18c6d71a9fe79e811c69f50a
amithkashyap <amithkashyaprv@gmail.com>
2024-06-12 14:38:28 +0530

    autowired product service in the controller

--- src/main/java/com/ecommerce/productservice/controller/ProductController.java
+++ src/main/java/com/ecommerce/productservice/controller/ProductController.java
@@ -1,33 +1,42 @@
 package com.ecommerce.productservice.controller;
 
+import com.ecommerce.productservice.service.ProductServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/products")
 @RestController
 public class ProductController {
 
+    private ProductServiceImpl productServiceImpl;
+
+    @Autowired
+    public ProductController(ProductServiceImpl productServiceImpl) {
+        this.productServiceImpl = productServiceImpl;
+    }
+
     @GetMapping
     public String getAllProducts() {
-        return "all products";
+        return productServiceImpl.getAllProducts();
     }
 
     @GetMapping("/{id}")
     public String getProductById(@PathVariable("id") Long id) {
-        return "Product id : " + id;
+        return productServiceImpl.getProductById(id);
     }
 
     @PostMapping("/")
     public String addProduct() {
-        return "adding product";
+        return productServiceImpl.addProduct();
     }
 
     @PutMapping("/{id}")
     public String updateProduct(@PathVariable("id") Long id) {
-        return "updating product : " + id;
+        return productServiceImpl.updateProduct(id);
     }
 
     @DeleteMapping("/{id}")
     public String deleteProductById(@PathVariable("id") Long id) {
-        return "deleting product : " + id;
+        return productServiceImpl.deleteProductById(id);
     }
 }
