5800f4e54fba95ba0cb68c57646b8b49123f405a
amithkashyap <amithkashyaprv@gmail.com>
2024-06-12 14:46:42 +0530

    implementing service interface

--- src/main/java/com/ecommerce/productservice/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/productservice/service/ProductServiceImpl.java
@@ -3,7 +3,7 @@ package com.ecommerce.productservice.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService {
 
     public String getAllProducts() {
         return "all products";
