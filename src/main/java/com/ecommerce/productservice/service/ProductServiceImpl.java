bf83474ec3b8fe592780e6551896b93b0564b541
amithkashyap <amithkashyaprv@gmail.com>
2024-06-15 01:55:00 +0530

    refactored product service impl class

--- src/main/java/com/ecommerce/productservice/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/productservice/service/ProductServiceImpl.java
@@ -1,27 +1,34 @@
 package com.ecommerce.productservice.service;
 
+import com.ecommerce.productservice.entity.Product;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.util.List;
+
+@Service("customProductService")
 public class ProductServiceImpl implements ProductService {
-
-    public String getAllProducts() {
-        return "all products";
+    @Override
+    public List<Product> getAllProducts() {
+        return null;
     }
 
-    public String getProductById(Long id) {
-        return "Product id : " + id;
+    @Override
+    public Product getProductById(Long id) {
+        return null;
     }
 
-    public String addProduct() {
-        return "adding product";
+    @Override
+    public Product addProduct(Product product) {
+        return null;
     }
 
+    @Override
     public String updateProduct(Long id) {
         return "updating product : " + id;
     }
 
-    public String deleteProductById(Long id) {
-        return "deleting product : " + id;
+    @Override
+    public Product deleteProductById(Long id) {
+        return null;
     }
 }
