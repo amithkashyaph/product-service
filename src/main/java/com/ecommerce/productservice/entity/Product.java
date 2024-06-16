ea559a5d3c9a5c813bf7771d382a875b863ea725
amithkashyap <amithkashyaprv@gmail.com>
2024-06-16 19:58:22 +0530

    added product entity

--- src/main/java/com/ecommerce/productservice/entity/Product.java
+++ src/main/java/com/ecommerce/productservice/entity/Product.java
@@ -1,17 +1,19 @@
 package com.ecommerce.productservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+public class Product extends BaseModel {
 
-public class Product {
-
-    private Long id;
     private String title;
     private String description;
+    @ManyToOne
     private Category category;
-    private Float price;
+    private Long price;
     private Float rating;
 }
