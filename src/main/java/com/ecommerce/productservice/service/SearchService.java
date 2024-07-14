b7d328956d60eee2b8925f6e5f17e8582b4e5b29
amithkashyap <amithkashyaprv@gmail.com>
2024-07-14 13:16:08 +0530

    added initial code for search API service

--- src/main/java/com/ecommerce/productservice/service/SearchService.java
+++ src/main/java/com/ecommerce/productservice/service/SearchService.java
@@ -1,7 +1,14 @@
 package com.ecommerce.productservice.service;
 
+import com.ecommerce.productservice.dtos.GenericProductDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SearchService {
+
+    public List<GenericProductDto> getAllProductsBySearchTerm(String query) {
+        return null;
+    }
 }
