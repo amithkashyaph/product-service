d9a5c9fda2f9da7eb1727d027c597677727a172b
amithkashyap <amithkashyaprv@gmail.com>
2024-07-14 14:35:30 +0530

    added test data via command line runner

--- src/main/java/com/ecommerce/productservice/ProductServiceApplication.java
+++ src/main/java/com/ecommerce/productservice/ProductServiceApplication.java
@@ -12,13 +12,20 @@ package com.ecommerce.productservice;
 //import com.ecommerce.productservice.inheritancemapping.joinedtable.repository.MentorRepository;
 //import com.ecommerce.productservice.inheritancemapping.joinedtable.repository.StudentRepository;
 //import com.ecommerce.productservice.inheritancemapping.joinedtable.repository.UserRepository;
+import com.ecommerce.productservice.entity.Category;
+import com.ecommerce.productservice.entity.Price;
+import com.ecommerce.productservice.entity.Product;
 import com.ecommerce.productservice.inheritancemapping.singletable.Mentor;
 import com.ecommerce.productservice.inheritancemapping.singletable.Student;
 import com.ecommerce.productservice.inheritancemapping.singletable.User;
 import com.ecommerce.productservice.inheritancemapping.singletable.repository.MentorRepository;
 import com.ecommerce.productservice.inheritancemapping.singletable.repository.StudentRepository;
 import com.ecommerce.productservice.inheritancemapping.singletable.repository.UserRepository;
+import com.ecommerce.productservice.repositories.CategoryRepository;
+import com.ecommerce.productservice.repositories.PriceRepository;
+import com.ecommerce.productservice.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -39,6 +46,19 @@ public class ProductServiceApplication implements CommandLineRunner {
 
 	@Qualifier("joinedUserRepository")
 	private UserRepository userRepository;
+
+	private PriceRepository priceRepository;
+
+	private CategoryRepository categoryRepository;
+
+	private ProductRepository productRepository;
+
+	@Autowired
+	public ProductServiceApplication(ProductRepository productRepository, PriceRepository priceRepository, CategoryRepository categoryRepository) {
+		this.productRepository = productRepository;
+		this.categoryRepository = categoryRepository;
+		this.priceRepository = priceRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
@@ -73,7 +93,57 @@ public class ProductServiceApplication implements CommandLineRunner {
 //
 //		System.out.println(users);
 
-
+//		Category category = new Category();
+//		category.setName("mobile");
+//		Category mobileCategory = categoryRepository.save(category);
+//
+//		Category category2 = new Category();
+//		category2.setName("laptop");
+//		Category laptopCategory = categoryRepository.save(category2);
+//
+//		Category category3 = new Category();
+//		category3.setName("desktop");
+//		Category desktopCategory = categoryRepository.save(category3);
+//
+//		Price price = new Price();
+//		price.setCurrency("INR");
+//		price.setValue(1000d);
+//		Price savedPrice = priceRepository.save(price);
+//
+//		Price price2 = new Price();
+//		price2.setCurrency("INR");
+//		price2.setValue(2000d);
+//		Price savedPrice2 = priceRepository.save(price2);
+//
+//		Price price3 = new Price();
+//		price3.setCurrency("INR");
+//		price3.setValue(2000d);
+//		Price savedPrice3 = priceRepository.save(price3);
+//
+//		Product product = new Product();
+//		product.setTitle("Iphone 13");
+//		product.setDescription("Good iPhone 13 phone");
+//		product.setCategory(mobileCategory);
+//		product.setPrice(savedPrice);
+//		product.setRating(4.4f);
+//		productRepository.save(product);
+//
+//		Product product2 = new Product();
+//		product2.setTitle("Iphone 14");
+//		product2.setDescription("Good iPhone 14 phone");
+//		product2.setCategory(mobileCategory);
+//		product2.setPrice(savedPrice2);
+//		product2.setRating(4.7f);
+//		productRepository.save(product2);
+//
+//
+//		Product product3 = new Product();
+//		product3.setTitle("Iphone 15");
+//		product3.setDescription("Good iPhone 15 phone");
+//		product3.setCategory(mobileCategory);
+//		product3.setPrice(savedPrice3);
+//		product3.setRating(4.9f);
+//		productRepository.save(product3);
 
 	}
 }
