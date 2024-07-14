7033d130b64bb870965d70c83fbc8b58a579f6b6
amithkashyap <amithkashyaprv@gmail.com>
2024-07-14 12:55:05 +0530

    implemented findAllByPrice_ValueBetween API in product repository interface

--- src/main/java/com/ecommerce/productservice/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/productservice/repositories/ProductRepository.java
@@ -18,4 +18,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllByPrice_ValueLessThan(Integer priceValue);
 
+    List<Product> findAllByPrice_ValueBetween(Integer basePrice, Integer maxPrice);
+
 }
