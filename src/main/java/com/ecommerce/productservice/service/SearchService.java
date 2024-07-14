b3042e3150ca3219b036ebcdedab1d9597198f32
amithkashyap <amithkashyaprv@gmail.com>
2024-07-14 13:19:00 +0530

    implemented search API service layer

--- src/main/java/com/ecommerce/productservice/service/SearchService.java
+++ src/main/java/com/ecommerce/productservice/service/SearchService.java
@@ -1,14 +1,31 @@
 package com.ecommerce.productservice.service;
 
 import com.ecommerce.productservice.dtos.GenericProductDto;
+import com.ecommerce.productservice.entity.Product;
+import com.ecommerce.productservice.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class SearchService {
 
+    private ProductRepository productRepository;
+
+    @Autowired
+    public SearchService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     public List<GenericProductDto> getAllProductsBySearchTerm(String query) {
-        return null;
+        List<Product> productList = productRepository.findAllByTitle(query);
+
+        List<GenericProductDto> genericProductDtoList = new ArrayList<>();
+        for(Product product: productList) {
+            genericProductDtoList.add(Product.from(product));
+        }
+        return genericProductDtoList;
     }
 }
