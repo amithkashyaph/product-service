a8c4530b96ea674956f8ac07ec1af22973e8f9a6
amithkashyap <amithkashyaprv@gmail.com>
2024-07-14 13:14:49 +0530

    added initial code for search API

--- src/main/java/com/ecommerce/productservice/controller/SearchController.java
+++ src/main/java/com/ecommerce/productservice/controller/SearchController.java
@@ -1,7 +1,18 @@
 package com.ecommerce.productservice.controller;
 
+import com.ecommerce.productservice.dtos.GenericProductDto;
+import com.ecommerce.productservice.dtos.SearchRequestDto;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/search")
 public class SearchController {
+
+    public List<GenericProductDto> getAllProductsForSearchTerm(@RequestBody SearchRequestDto searchRequestDto) {
+        return null;
+    }
 }
