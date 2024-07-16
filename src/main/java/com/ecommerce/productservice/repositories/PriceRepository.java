6025c27940b0a4a4cc97914da44b0c25a9feefad
amithkashyap <amithkashyaprv@gmail.com>
2024-07-17 00:24:11 +0530

    added price repository interface

--- src/main/java/com/ecommerce/productservice/repositories/PriceRepository.java
+++ src/main/java/com/ecommerce/productservice/repositories/PriceRepository.java
@@ -1,4 +1,9 @@
 package com.ecommerce.productservice.repositories;
 
-public interface PriceRepository {
+import com.ecommerce.productservice.entity.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PriceRepository extends JpaRepository<Price, Long> {
 }
