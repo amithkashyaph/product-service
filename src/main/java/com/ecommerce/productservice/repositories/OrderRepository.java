2fd6c1f285e4f2f286c47638640ccbea8cbed116
amithkashyap <amithkashyaprv@gmail.com>
2024-07-17 00:24:00 +0530

    added order repository interface

--- src/main/java/com/ecommerce/productservice/repositories/OrderRepository.java
+++ src/main/java/com/ecommerce/productservice/repositories/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.ecommerce.productservice.repositories;
 
-public interface OrderRepository {
+import com.ecommerce.productservice.entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Orders, Long> {
 }
