a3452d34920ebbf937a32fe39415b9c28cf27049
amithkashyap <amithkashyaprv@gmail.com>
2024-07-21 12:24:00 +0530

    updated service component dependency

--- src/main/java/com/ecommerce/productservice/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/productservice/service/ProductServiceImpl.java
@@ -1,12 +1,18 @@
 package com.ecommerce.productservice.service;
 
 import com.ecommerce.productservice.entity.Product;
+import com.ecommerce.productservice.repositories.ProductRepository;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("customProductService")
+@Primary
 public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
     @Override
     public List<Product> getAllProducts() {
         return null;
