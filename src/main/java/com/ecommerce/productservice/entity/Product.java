365a50bfa11107d3852df2d62bf1819bfb3a9181
amithkashyap <amithkashyaprv@gmail.com>
2024-07-25 02:14:01 +0530

    implemented product dto mapper logic

--- src/main/java/com/ecommerce/productservice/entity/Product.java
+++ src/main/java/com/ecommerce/productservice/entity/Product.java
@@ -1,9 +1,14 @@
 package com.ecommerce.productservice.entity;
 
+import com.ecommerce.productservice.dtos.GenericProductDto;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,9 +16,28 @@ import lombok.Setter;
 public class Product extends BaseModel {
 
     private String title;
+
     private String description;
+
     @ManyToOne
     private Category category;
-    private Long price;
+
+    @OneToOne
+    private Price price;
+
     private Float rating;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Orders> orders;
+
+    public static GenericProductDto from(Product product) {
+        GenericProductDto genericProductDto = new GenericProductDto();
+        genericProductDto.setId(product.getId());
+        genericProductDto.setTitle(product.getTitle());
+        genericProductDto.setDescription(product.getDescription());
+        genericProductDto.setCategory(product.getCategory().getName());
+        genericProductDto.setPrice(product.getPrice().getValue().intValue());
+
+        return genericProductDto;
+    }
 }
