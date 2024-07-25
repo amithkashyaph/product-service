9794d8ba10a84a5e3ac3f401bd84f2490ac30f7d
amithkashyap <amithkashyaprv@gmail.com>
2024-07-26 01:12:11 +0530

    implemented product controller test class

--- src/test/java/com/ecommerce/productservice/controller/ProductControllerTest.java
+++ src/test/java/com/ecommerce/productservice/controller/ProductControllerTest.java
@@ -1,27 +1,37 @@
 package com.ecommerce.productservice.controller;
 
+import com.ecommerce.productservice.entity.Product;
 import com.ecommerce.productservice.service.ProductService;
-import jakarta.inject.Inject;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.util.AssertionErrors;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
+
 
 @SpringBootTest
 public class ProductControllerTest {
 
     @MockBean
+    @Qualifier("customProductService")
     private ProductService productService;
 
-    @Inject
+    @Autowired
     private ProductController productController;
 
+    @Test
     void testGetAllProductsWhenListIsEmpty() {
-        when(productService.getAllProducts()).thenReturn(new ArrayList<>());
+        when(productService.getAllProducts()).thenReturn(Collections.emptyList());
 
+        List<Product> productList = productController.getAllProducts();
+
+        Assertions.assertTrue(productController.getAllProducts().isEmpty());
     }
 }
